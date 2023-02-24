f9ed433be48718179318f7e763fe89e2247c2cc6
Mees <meesjvdhulst@outlook.com>
2023-02-24 11:36:26 +0100

    buttons toegevoegd home page
    login button en registreer button toegevoegd aan index.php form gebruikt omdat dat mooier eruit ziet :)

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
-<?php
-echo "tesssttt";
-?>
+<!DOCTYPE html>
+<form action="login.php">
+    <input type="submit" value="login" />
+</form>
+</br>
+<form action="registratie.php">
+    <input type="submit" value="registreren" />
+</form>
